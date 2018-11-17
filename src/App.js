646e51e2927cf18e3c22e63729452ae5ce401659
alexandercortez <joelalexander10@outlook.com>
2018-11-16 21:54:37 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  componentWillMount() {
+    const data = {
+      "api_key": "d5c5b2943bde1c34591b8e6009d085ad",
+      "user_id": 2305,
+      "report": {
+        "type": "stats",
+        "start_date": "2014-08-03",
+        "end_date": "2014-08-03",
+        "columns": [
+          "imp",
+          "leads",
+          "money"
+        ],
+        "orders": [
+          "imp",
+          "leads"
+        ],
+        "group": [
+          "hour"
+        ],
+        "filters": {
+          "father_id": [
+            "123456"
+          ],
+          "countries": [
+            "AR",
+            "ES"
+          ]
+        }
+      }
+    };
+    axios.defaults.headers = {
+
+    }
+    // axios.get('https://api.leadzu.com/report', {
+    axios.post('http://api.leadzu.com/report', data)
+      .then((response) => {
+        console.log('response =>', response)
+      })
+      .catch((err) => {
+        console.log('err =>', err);
+      })
+  }
   render() {
     return (
       <div className="App">
